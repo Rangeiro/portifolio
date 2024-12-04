8e6a83c68538aaf41e093769e5c0438c188555b5
Rangeiro <manuelsegurarranjeirojunior@gmail.com>
2024-12-04 08:40:26 +0100

    navbar-responsiva

--- script.js
+++ script.js
@@ -2,16 +2,18 @@ let icon = document.getElementById('icon')
 let nav_bar = document.getElementById('nav-bar')
 
 
-function hamburger(){
-    nav_bar.style.display = 'none'
-    icon.style.display = 'block'
-    
-    if(nav_bar.style.display == 'none'){
-        nav_bar.style.display = 'flex'
-        icon.style.display = 'none'
+if (nav_bar.style.width >= `600px`){
+    function hamburger(){
+        nav_bar.style.display = 'none'
+        icon.style.display = 'block'
+        
+        if(nav_bar.style.display == 'none'){
+            nav_bar.style.display = 'flex'
+            icon.style.display = 'none'
+        }
     }
-}
-function repor(){
-    nav_bar.style.display = 'none'
-    icon.style.display = 'block'
+    function repor(){
+        nav_bar.style.display = 'none'
+        icon.style.display = 'block'
+    }
 }
